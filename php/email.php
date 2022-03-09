c2915dbf5b0510db1f00f06a16c4e88da1f6c032
stonedhesus <ionescu_paul@outlook.com>
2022-03-09 12:59:08 +0100

    Commenced working on adding a blog section to the, what is slowly becoming a web application; and in order to spare some time I had decided to re-utilise an old project of mine, which initially was a Django based web application, and thus I had slowly started the conversion process from the template format which the blog had to the PHP friendlier one.

--- php/email.php
+++ php/email.php
@@ -6,5 +6,11 @@
         $header .= "Organisation: ".$_POST["organisation"]."\r\n";
         $header .= "Full name: ".$_POST["firstname"]." ".$_POST["lastname"]."\r\n";
 
-        mail("test", $_POST["subject"], $_POST["message"],$header);
+        $object = 'Testing';
+        $text   = 'Just testing';
+
+        if(mail('andrei.ionescu@etu-u.paris.fr', $object, $text)){
+
+
+        }
     }
