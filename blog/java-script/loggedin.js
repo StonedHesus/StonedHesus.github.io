e56e83963f1b130980cba8a865c0b2e39ec332ba
stonedhesus <ionescu_paul@outlook.com>
2022-03-14 15:22:03 +0100

    I masked the blog button on the version which is uploaded so as to ensure a proper behaviour of the page, one which will not mystify the viewers.

---
+++ blog/java-script/loggedin.js
@@ -0,0 +1,8 @@
+const addArticleButton = document.getElementsByClassName("logged-user");
+
+
+const cookie = document.cookie;
+
+console.log(cookie);
+
+addArticleButton.item(0).setAttribute("style", "visibility:hidden");
